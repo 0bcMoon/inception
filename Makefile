ca6f1e7a5931a19d96ad292196e2d281df0156e1
Hicham Benoukaiss <hibenouk@e1r5p14.1337.ma>
2025-11-04 19:20:57 +0100

    use::ai

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 all: up
 
 up: volumes
-	docker-compose up -d
+	DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker-compose up -d --build --parallel
 down:
 	docker-compose down
 
--- src/requirements/wordpress/build.sh
+++ src/requirements/wordpress/build.sh
@@ -6,13 +6,13 @@ sed -i 's/memory_limit = 128M/memory_limit = 512M/' /etc/php83/php.ini
 
 mkdir -p /run/php
 
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
 
-wp core download --skip-content --force
+wp core download --skip-content --force --quiet
 
 mv wp-config-sample.php wp-config.php 
 
