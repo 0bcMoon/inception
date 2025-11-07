2435bb912195713d2a4aa7b29497b40103de132e
0bcMoon <hichambenoukaiss0@gmail.com>
2025-11-07 20:29:26 +0000

    fix ftp

--- src/requirements/bonus/ftp/setup.sh
+++ src/requirements/bonus/ftp/setup.sh
@@ -6,4 +6,6 @@ adduser -D -h /srv/ftp/wordpress -s /sbin/nologin $FTP_USER
 
 echo "$FTP_USER:$FTP_PASS" | chpasswd
 
+sed -i "s/IP_ADDR/$FTP_ADDR/1" /etc/vsftpd/vsftpd.conf
 exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+
